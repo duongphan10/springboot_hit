0f5eca608eeba0f0ff3bc209ab83296e5fc50d08
Duong Phan <92658521+duongphan10@users.noreply.github.com>
2023-05-08 15:10:09 +0700

    add Buoi3_BTVN

---
+++ Buoi3_BTVN/src/test/java/com/example/buoi3_btvn/Buoi3BtvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buoi3_btvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi3BtvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
